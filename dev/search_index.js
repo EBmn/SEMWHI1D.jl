53806c94dd020d433042ab0cfb94a14f6c113445
Documenter.jl <documenter@juliadocs.github.io>
2024-10-03 19:04:20 +0000

    build based on f4765d0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SEMWHI1D","category":"page"},{"location":"#SEMWHI1D","page":"Home","title":"SEMWHI1D","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SEMWHI1D.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SEMWHI1D]","category":"page"}]
+}
